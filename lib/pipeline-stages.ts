5e888a7108f31e69b5bf3d8de13cc386e15c7d37
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-03-05 16:27:40 +0200

    wtf

---
+++ lib/pipeline-stages.ts
@@ -0,0 +1,11 @@
+import { CdkPipeStack } from "./cdk-pipe-stack";
+import { Stage, StageProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class PipelineStage extends Stage {
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
+
+    new CdkPipeStack(this, "WebService");
+  }
+}
